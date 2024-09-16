8b268c0c4b83cd3c6a77fe25734397fbca682384
Andre <andreluizmartinstavares@gmail.com>
2024-09-16 17:32:52 -0300

    aplicando if e modificando celulas

--- sokoban.js
+++ sokoban.js
@@ -1,12 +1,26 @@
 const board2 = document.getElementById('board2');
-
+const M = 8;
+const N = 8;
+for(let k=0; k<M; k++) {
 const row = document.createElement('div'); 
 row.classList.add('row');
-
-for (let i=0; i<4; i++) {
+   
+for (let i=0; i<N; i++) {
     const cell = document.createElement('div');   
     cell.classList.add('cell');
-    board2.appendChild(cell); 
-}
-
+    row.appendChild(cell); 
+    
+//     if (k == 0 || k == M-1 || i == 0 || i == N-1) {
+//         cell.classList.add('dir');
+//    }
+    if (k > 0 && k < M-1 && i > 0 && i < N-1) 
+        cell.classList.add('dir');
+    
 board2.append(row);
+}
+   
+}
+//  const celldir = document.createElement('div');
+//     row.classList.add('cell', 'dir');
+//     row.appendChild(celldir);
+//     board2.append(row)
