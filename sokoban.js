93d0f592b171f1ac7ee6ef63dd11f1322858ae6a
Andre <andreluizmartinstavares@gmail.com>
2024-09-23 17:24:50 -0300

    outro metodo de movimentação

--- sokoban.js
+++ sokoban.js
@@ -2,6 +2,12 @@ const player = new Player(0, 0);
 const celulas = document.querySelectorAll('.cell');
 const element = document.querySelector('.player');
 
+const DIST_SALTO = 66;
+const MARGIN_FIX = 4;
+
+element.style.top = calculaPosicao(0);
+element.style.left = calculaPosicao(0);
+
 window.addEventListener("keydown", function (event) {
     const next = player.nextPosition(event.code);
 
@@ -25,11 +31,12 @@ function Player(x, y) {
         return { x, y };
     }
 
-    this.moveTo = function(position, element, parent) {
+    this.moveTo = function(position, element, _parent) {
         this.x = position.x;
         this.y = position.y;
 
-        parent.append(element);
+        element.style.top = calculaPosicao(this.x);
+        element.style.left = calculaPosicao(this.y);
     }
 }
 
@@ -38,3 +45,14 @@ function verifyPosition(position) {
 
     return x >= 0 && x < 8 && y >= 0 && y < 8;
 }
+
+function calculaPosicao(qtd) {
+
+    return  (qtd * DIST_SALTO + MARGIN_FIX + "px");
+}
+
+console.log(calculaPosicao(0, 64) === "0px");
+console.log(calculaPosicao(1, 64) === "64px");
+console.log(calculaPosicao(2, 32) === "64px");
+console.log(calculaPosicao(10, 60) === "600px");
+console.log(calculaPosicao(-3, 45) === "-135px");
