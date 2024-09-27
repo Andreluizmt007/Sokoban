5e15c6ee3ab591c60ac16effe141d4021de5f3e5
Andre <andreluizmartinstavares@gmail.com>
2024-09-27 17:32:42 -0300

    Desafio

--- board.js
+++ board.js
@@ -13,7 +13,9 @@ const boardMap = [
 function buildGameBoard(numberOfRows, numberOfCollumns) {
     const game = document.getElementById("game");
     const board = createGameElement('div', 'board', game);
-    const pieces = {}
+    const pieces = {
+        boxes: []
+    };
     
     for (let x = 0; x < numberOfRows; x++) {
         const row = createGameElement('div', 'row', board);
@@ -23,9 +25,9 @@ function buildGameBoard(numberOfRows, numberOfCollumns) {
             const char =  boardMap[x][y];
 
             if(char === '#')cell.classList.add('wall');
-            if(char === 'B')cell.classList.add('box');
             if(char === 'G')cell.classList.add('goal');
             if(char === 'P')pieces.player = {x: x, y: y};
+            if(char === 'B')pieces.boxes.push({x: x, y: y});
         }
     } 
     return pieces;
