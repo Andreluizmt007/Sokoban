b8aee04b49bf0e5a3a91d27cd247d35357a826b1
Andre <andreluizmartinstavares@gmail.com>
2024-09-27 17:07:12 -0300

    move function calculaPosicao

--- pieces.js
+++ pieces.js
@@ -1,3 +1,6 @@
+const DIST_SALTO = 66;
+const MARGIN_FIX = 4;
+
 function Piece(x, y) {
     this.x = x;
     this.y = y;
@@ -25,5 +28,10 @@ function Piece(x, y) {
 
         this.element.style.top = calculaPosicao(this.x);
         this.element.style.left = calculaPosicao(this.y);
+    }
+    
+    function calculaPosicao(qtd) {
+    
+        return (qtd * DIST_SALTO + MARGIN_FIX + "px");
     }
 }
