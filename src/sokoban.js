c21629991bbc2406e22ba04ea3e004d423f6293d
Andre <andreluizmartinstavares@gmail.com>
2024-10-03 17:34:12 -0300

    mapa 2

--- src/sokoban.js
+++ src/sokoban.js
@@ -1,8 +1,8 @@
 import Piece from "./pieces.js";
 import { buildGameBoard } from "./board.js";
-import { lvl0 } from "./level.js";
+import { lvl0, lvl1 } from "./level.js";
 
-const { boardMap, pieces, numberOfGoals } = buildGameBoard(lvl0);
+const { boardMap, pieces, numberOfGoals } = buildGameBoard(lvl1);
 const board = document.querySelector('.board');
 
 const player = createBoardPiece(pieces.player, 'player');
