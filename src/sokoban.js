821704e5750e9e84e5b2ee488232893f5d7c012a
Andreluizmt007 <andreluizmartinstavares@gmail.com>
2025-01-09 16:01:40 +0000

    fase0

--- src/sokoban.js
+++ src/sokoban.js
@@ -1,7 +1,7 @@
 import { buildGameBoard } from "./board.js";
 import { lvl0, lvl1, lvl2 } from "./level.js";
 
-const { boardMap, pieces: { boxes, player }, numberOfGoals } = buildGameBoard(lvl1);
+const { boardMap, pieces: { boxes, player }, numberOfGoals } = buildGameBoard(lvl0);
 
 window.addEventListener("keydown", function (event) {
     handlePieceMovement(event.code);
